af95d301714949bf02303583aa5842dbe6ab48ca
Miles <milesvanderlely@gmail.com>
2018-11-05 14:24:58 +0100

    ReturnTwelve Function Added

--- SimpleProject/Source.cpp
+++ SimpleProject/Source.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+const int & returnTwelve(){
+	const static int & twelve = 12;
+	return twelve;
+}
+
 int main(int args, char ** cargs) {
-	printf("Hello world!");
+	printf("Twelve is %d\n", returnTwelve());
+
 	return 0;
 }
